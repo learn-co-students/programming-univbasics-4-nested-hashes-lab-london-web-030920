2bddccd3268fef7d7553dd5a0883d5614ccaf263
Julian Wakefield <julianwakefield@outlook.com>
2020-03-01 16:57:00 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -82,7 +82,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         },
-				:yukihiro_matsumoto => {}
+				:yukihiro_matsumoto => {
+						:known_for => "Ruby",
+						:languages => ["LISP", "C"]
+				}
 
      }
 
